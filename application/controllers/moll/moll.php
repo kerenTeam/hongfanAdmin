ce3f9043c70b8336f8f618494a0f04187f9351de
muzishuiji <2510909248@qq.com>
2016-12-02 09:33:33 +0800

    no message

--- application/controllers/moll/moll.php
+++ application/controllers/moll/moll.php
@@ -13,24 +13,34 @@ class moll extends CI_Controller {
 
     }
 
-    //商场 基础配置
-    function index()
-    {
-    	 $this->load->view('moll/baseSet.html');
-    }
-    //商场 基础信息
-    function baseInfo()
-    {
-         $this->load->view('moll/baseInfo.html');
-    }
-    //业态配置
-    function yetai(){
+    // //商场 基础配置
+    // function index()
+    // {
+    // 	 $this->load->view('moll/mollBaseSet.html');
+    // }
+    // //商场 基础信息
+    // function baseInfo()
+    // {
+    //      $this->load->view('moll/mollBaseInfo.html');
+    // }
+    //业态列表
+    function mollyetaiList(){
 
-    	 $this->load->view('moll/yetai.html');
+    	 $this->load->view('moll/mollyetaiList.html');
+    }
+    //新增业态
+    function mollAddYetai(){
+
+         $this->load->view('moll/mollAddYetai.html');
+    }
+     //编辑业态
+    function mollEditYetai(){
+
+         $this->load->view('moll/mollEditYetai.html');
     }
     //楼层信息
-    function floorInfo(){
-        $this->load->view('moll/floorInfo.html');
+    function mollFloorInfo(){
+        $this->load->view('moll/mollFloorInfo.html');
     }
     //商场简介
     function mollBrief(){
