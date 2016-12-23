d15873341dc39a8f8fc9e7aa38b9af2e8ccd85b2
muzishuiji <2510909248@qq.com>
2016-12-23 17:22:48 +0800

    no message

--- application/controllers/lovetogo/loveToGo.php
+++ application/controllers/lovetogo/loveToGo.php
@@ -10,19 +10,34 @@ class loveToGo extends default_Controller
 {
 	public $view_loveToGoList = 'loveToGo/loveToGoList.html';	
 	public $view_loveToGogoodDetail = 'loveToGo/loveToGogoodDetail.html';
+    public $view_loveToGoListLocal = 'loveToGo/loveToGoListLocal.html';   
+    public $view_loveToGogoodLocalDetail = 'loveToGo/loveToGogoodLocalDetail.html';
 	function __construct()
 	{
 		 parent::__construct();
 	}
+    //爱购 本地商品
+    function loveToGoListLocal(){
+        $data['page'] = $this->view_loveToGoListLocal;
+        $data['menu'] = array('loveToGo','loveToGoListLocal');
+        $this->load->view('template.html',$data);
+    }
 
-	//爱购 商品列表
+    //爱购 本地商品详情
+    function loveToGogoodLocalDetail(){
+        $data['page'] = $this->view_loveToGogoodDetail;
+        $data['menu'] = array('loveToGo','loveToGogoodLocalDetail');
+        $this->load->view('template.html',$data);
+    }
+
+	//爱购 商品库
     function loveToGoList(){
         $data['page'] = $this->view_loveToGoList;
         $data['menu'] = array('loveToGo','loveToGoList');
         $this->load->view('template.html',$data);
     }
 
-	//爱购 商品详情
+	//爱购 商品库 商品详情
     function loveToGogoodDetail(){
         $data['page'] = $this->view_loveToGogoodDetail;
         $data['menu'] = array('loveToGo','loveToGogoodDetail');
