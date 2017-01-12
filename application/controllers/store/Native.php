7f41b9723d2150d6d8e8af652acc1108e1336fdb
muzishuiji <2510909248@qq.com>
2017-01-12 12:15:24 +0800

    no message

--- application/controllers/store/Native.php
+++ application/controllers/store/Native.php
@@ -8,20 +8,28 @@ require_once(APPPATH.'controllers/Default_Controller.php');
 
 class Native extends Default_Controller {
 
+    //土特产商品列表
+    public $view_native_index = "store/native_index.html";
+    //土特产订单列表
+    public $view_native_order = "store/native_order.html";
     function __construct()
     {
          parent::__construct();
     }
 
-    //
+    //土特产商品列表
     function native_index(){
 
-        echo "23456";
+        $data['page'] = $this->view_native_index;
+        $data['menu'] = array('native','native_index');
+        $this->load->view('template.html',$data);
     }
 
-    //
+    //土特产订单列表
     function native_order(){
-        echo "2";
+        $data['page'] = $this->view_native_index;
+        $data['menu'] = array('native','native_order');
+        $this->load->view('template.html',$data);
     }
 
 }
