93e882b825382855595c166464b6857a4a3aa522
muzishuiji <2510909248@qq.com>
2017-04-05 18:17:45 +0800

    no message

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -14,9 +14,11 @@ class Admin extends Default_Controller
 	}
 
 	function index(){
+            if(isset($_SESSION['businessId'])){
                 if($_SESSION['users']['user_id'] != $_SESSION['businessId']){
                    unset($_SESSION['businessId']);
                 }
+            }
                 //获取用户登录次数
                 $query = $this->db->where('userid',$_SESSION['users']['user_id'])->where('login_address !=','')->order_by('create_time','desc')->get('hf_system_journal');
                 $login = $query->result_array();
