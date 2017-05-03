ff22f79f1e2355a5e67fae171d65e31414bc976a
424138799 <424138799@qq.com>
2017-05-03 17:42:35 +0800

    3456

---
+++ application/controllers/finance/Finance.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once(APPPATH.'controllers/Default_Controller.php');
+/*
+*   发现板块
+*/
+class Finance extends Default_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    function mallOrder(){
+        
+    }
+
+}
