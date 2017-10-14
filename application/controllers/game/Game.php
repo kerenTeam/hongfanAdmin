fae5f97f8c91e1fec21943b58ede888ed385d4b2
thefin <1425302858@qq.com>
2017-10-14 18:04:37 +0800

    no message

---
+++ application/controllers/game/Game.php
@@ -0,0 +1,43 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once(APPPATH.'controllers/Default_Controller.php');
+/**
+* 
+*/
+class Game extends Default_Controller
+{
+	
+
+	public $view_gameList = 'game/gameList.html';
+	public $view_gameInfo = 'game/gameInfo.html';
+
+
+	function __construct()
+	{
+		parent::__construct();	
+	}
+
+
+	function gameList(){
+
+		$data['page'] = $this->view_gameList;
+        $data['menu'] = array('game','gameList');
+        $this->load->view('template.html',$data);
+
+	}
+
+	function gameInfo(){
+
+		$data['page'] = $this->view_gameInfo;
+        $data['menu'] = array('game','gameList');
+        $this->load->view('template.html',$data);
+
+	}
+
+
+
+
+
+
+
+}
