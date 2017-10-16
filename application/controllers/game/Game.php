f0533f63e3b5914980b199f58ca9c7da1298dc76
424138799 <424138799@qq.com>
2017-10-16 10:00:26 +0800

    游戏

--- application/controllers/game/Game.php
+++ application/controllers/game/Game.php
@@ -16,18 +16,49 @@ class Game extends Default_Controller
 	function __construct()
 	{
 		parent::__construct();	
+		$this->load->model('Game_model');
+
 	}
 
-
+	//游戏奖品
 	function gameList(){
+		//返回所有奖品
+		$data['prize'] = $this->Game_model->select_prize('2');
 
 		$data['page'] = $this->view_gameList;
         $data['menu'] = array('game','gameList');
         $this->load->view('template.html',$data);
 
 	}
+	//删除游戏奖品
+	function del_prize(){
+		if($_POST){
+			$id = $this->input->post('id');
+			if($this->Game_model->del_prize($id)){
+				$log = array(
+                    'userid'=>$_SESSION['users']['user_id'],  
 
+                    "content" => $_SESSION['users']['username']."删除了游戏奖品，奖品ID是：".$id,
+                    "create_time" => date('Y-m-d H:i:s'),
+
+                    "userip" => get_client_ip(),
+
+                );
+                $this->db->insert('hf_system_journal',$log);
+				echo "1";
+			}else{
+				echo "2";
+			}
+		}else{
+			echo "2";
+		}
+	}
+
+	//y=游戏内容
 	function gameInfo(){
+		//获取游戏详情
+		$data['game'] = $this->Game_model->select_game_info('2');
+		
 
 		$data['page'] = $this->view_gameInfo;
         $data['menu'] = array('game','gameList');
@@ -35,7 +66,134 @@ class Game extends Default_Controller
 
 	}
 
+	//编辑游戏详情
+	function edit_game(){
+		if($_POST){
+			$data= $this->input->post();
+
+			$header = array("token:".$_SESSION['token'],'city:'.'1');    
+			if(!empty($_FILES['bgi']['name'])){
+                    $tmpfile = new CURLFile(realpath($_FILES['bgi']['tmp_name']));
+                
+                    $pics = array(
+                        'pics' =>$tmpfile,
+                        'porfix'=>'game/gamebgi',
+                        'bucket'=>'cqcother',
+                    );
+				
+					$qiuniu = json_decode(curl_post_express($header,QINIUUPLOAD,$pics),true);
+
+					if($qiuniu['errno'] == '0'){
+						$img = json_decode($qiuniu['data']['img'],true);
+						$icon[]['picImg'] =$img[0]['picImg'];
+						$data['BGI'] = json_encode($icon);
+					}else{
+					  echo "<script>alert('图片上传失败！');window.location.href='".site_url('/friends/Friends/circle')."'</script>";exit;
+					}
+            }
+            if(!empty($_FILES['fei']['name'])){
+                    $tmpfile = new CURLFile(realpath($_FILES['fei']['tmp_name']));
+                
+                    $pics = array(
+                        'pics' =>$tmpfile,
+                        'porfix'=>'game/gamebgi',
+                        'bucket'=>'cqcother',
+                    );
+				
+					$qiuniu = json_decode(curl_post_express($header,QINIUUPLOAD,$pics),true);
+
+					if($qiuniu['errno'] == '0'){
+						$img = json_decode($qiuniu['data']['img'],true);
+						$icon[]['picImg'] =$img[0]['picImg'];
+						$data['FEI'] = json_encode($icon);
+					}else{
+					  echo "<script>alert('图片上传失败！');window.location.href='".site_url('/game/Game/gameInfo')."'</script>";exit;
+					}
+            }
+
+            $data['startTime'] = strtotime($data['starttime']. ' 00:00:00')*1000;
+            $data['endTime'] = strtotime($data['endtime'].' 23:59:59')*1000;
+            unset($data['starttime'],$data['endtime']);
+            
+            if($this->Game_model->edit_game($data['id'],$data)){
+            	$log = array(
+                    'userid'=>$_SESSION['users']['user_id'],  
+
+                    "content" => $_SESSION['users']['username']."编辑了游戏详情",
+                    "create_time" => date('Y-m-d H:i:s'),
+
+                    "userip" => get_client_ip(),
+
+                );
+                $this->db->insert('hf_system_journal',$log);
+
+                echo "<script>alert('操作成功！');window.location.href='".site_url('/game/Game/gameInfo')."'</script>";exit;
+            }else{
+                echo "<script>alert('操作失败！');window.location.href='".site_url('/game/Game/gameInfo')."'</script>";exit;
+
+            }
+		}else{
+			$this->load->view('404.html');
+		}
+	}
+
+	//
 	function awardRecord(){
+		//返回所有中奖纪录
+		$config['per_page'] = 10;
+        //获取页码
+        $current_page=intval($this->uri->segment(4));//index.php 后数第4个/
+        //配置
+        $config['base_url'] = site_url('/game/Game/awardRecord');
+        //分页配置
+        $config['full_tag_open'] = '<ul class="am-pagination tpl-pagination">';
+
+        $config['full_tag_close'] = '</ul>';
+
+        $config['first_tag_open'] = '<li>';
+
+        $config['first_tag_close'] = '</li>';
+
+        $config['prev_tag_open'] = '<li>';
+
+        $config['prev_tag_close'] = '</li>';
+
+        $config['next_tag_open'] = '<li>';
+
+        $config['next_tag_close'] = '</li>';
+
+        $config['cur_tag_open'] = '<li class="am-active"><a>';
+
+        $config['cur_tag_close'] = '</a></li>';
+
+        $config['last_tag_open'] = '<li>';
+
+        $config['last_tag_close'] = '</li>';
+
+        $config['num_tag_open'] = '<li>';
+
+        $config['num_tag_close'] = '</li>';
+        $config['first_link']= '首页';
+
+        $config['next_link']= '下一页';
+
+        $config['prev_link']= '上一页';
+
+        $config['last_link']= '末页';
+        $list = $this->Game_model->select_history('2');
+
+        $config['total_rows'] = count($list);
+
+        // //分页数据
+        $listpage = $this->Game_model->select_history_page('2',$config['per_page'],$current_page);
+          $this->load->library('pagination');//加载ci pagination类
+
+            $this->pagination->initialize($config);
+
+        $data = array('lists'=>$listpage,'pages' => $this->pagination->create_links());
+
+
+
 
 		$data['page'] = $this->view_awardRecord;
         $data['menu'] = array('game','gameList');
